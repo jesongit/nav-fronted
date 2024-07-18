1c827528976ac509b27c86382507e1bc6b695ce6
posase <188380780@qq.com>
2024-07-18 23:44:35 +0800

    <feat>添加更多提示，添加谷歌搜索

--- src/components/AppLayout.vue
+++ src/components/AppLayout.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { ref, computed} from 'vue'
+import { ref, computed, onMounted } from 'vue'
 
 const text = ref('')
 const links = ref([])
+const input_type = ref('link')
 
 const search = computed<any>(() => {
-  if (text.value == '') return links.value
+  if (text.value == '' || input_type.value != 'link') return links.value
   return links.value.filter((item: any) => item.key.includes(text.value))
 })
 
@@ -14,10 +15,16 @@ function openLink(link: string) {
 }
 
 function handleKeydown(event: KeyboardEvent) {
-  if (event.key === 'Enter') {
+  if (event.key == 'Enter' && input_type.value == 'link') {
     openLink(search.value[0].link)
-  } else if (event.key === 'Escape') {
+  } else if (event.key == 'Enter' && input_type.value == 'search') {
+    openLink('https://www.google.com/search?q=' + text.value)
+  } else if (event.key == 'Escape') {
     text.value = ''
+  } else if (event.altKey && event.key == 'g') {
+    input_type.value = 'search'
+  } else if (event.altKey && event.key == 'l') {
+    input_type.value = 'link'
   }
 }
 
@@ -29,6 +36,8 @@ fetch('links.json')
   .catch(error => {
     console.log(error);
   });
+
+
 </script>
 
 <template>
@@ -37,16 +46,17 @@ fetch('links.json')
       <div class="row q-mt-md">
         <div class="col"></div>
         <div class="col-6">
-          <q-input autofocus outlined v-model="text" @keydown="handleKeydown" />
+          <q-input 
+          autofocus outlined 
+          placeholder="谷歌搜索: Alt+G  搜索导航: Alt+L"
+          v-model="text" @keydown="handleKeydown">
+            <template v-slot:prepend>
+              <q-icon :name="input_type" />
+            </template>
+          </q-input>
           <div class="row justify-center q-mt-sm q-gutter-sm">
-            <q-btn
-              outline
-              color="primary"
-              @click="openLink(link)"
-              :label="name"
-              v-for="{ key, name, link } in search"
-              :key="key"
-            />
+            <q-btn outline color="primary" @click="openLink(link)" :label="name" v-for="{ key, name, link } in search"
+              :key="key" />
           </div>
         </div>
         <div class="col"></div>
